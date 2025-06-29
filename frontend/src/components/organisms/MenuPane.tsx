7cebe8747749dc6177a32d1091e4f3707800e016
kmaruoka <k.m.igosso@gmail.com>
2025-06-29 14:31:20 +0900

    style: add vertical dividers in menu

--- frontend/src/components/organisms/MenuPane.tsx
+++ frontend/src/components/organisms/MenuPane.tsx
@@ -2,11 +2,19 @@ import CustomLink from '../atoms/CustomLink';
 
 export default function MenuPane() {
   return (
-    <nav className="p-2 bg-gray-200 flex space-x-4 fixed bottom-0 left-0 right-0 justify-around">
-      <CustomLink to="/">地図</CustomLink>
-      <CustomLink to="/catalog">図鑑</CustomLink>
-      <CustomLink to="/user">ユーザー</CustomLink>
-      <CustomLink to="/settings">設定</CustomLink>
+    <nav className="bg-gray-200 flex fixed bottom-0 left-0 right-0 divide-x divide-gray-300">
+      <CustomLink className="flex-1 py-2 text-center block" to="/">
+        地図
+      </CustomLink>
+      <CustomLink className="flex-1 py-2 text-center block" to="/catalog">
+        図鑑
+      </CustomLink>
+      <CustomLink className="flex-1 py-2 text-center block" to="/user">
+        ユーザー
+      </CustomLink>
+      <CustomLink className="flex-1 py-2 text-center block" to="/settings">
+        設定
+      </CustomLink>
     </nav>
   );
 }
