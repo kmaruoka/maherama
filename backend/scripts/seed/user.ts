802874bdd8e5671ec7fb1e1432f9544018c37c31
maruoka <maruoka@aqtor.co.jp>
2025-07-05 09:55:11 +0900

    fix(backend): testdata, .gitignore

--- backend/scripts/seed/user.ts
+++ backend/scripts/seed/user.ts
@@ -3,11 +3,11 @@ import { PrismaClient } from '@prisma/client';
 export async function seedUser(prisma: PrismaClient) {
   await prisma.user.createMany({
     data: [
-      { id: 1, name: 'テストユーザー1' },
-      { id: 2, name: 'テストユーザー2' },
-      { id: 3, name: 'テストユーザー3' },
-      { id: 4, name: 'テストユーザー4' },
-      { id: 5, name: 'テストユーザー5' },
+      { id: 1, name: 'まるにぃ' },
+      { id: 2, name: 'たかやん' },
+      { id: 3, name: 'ダイナマイト古川' },
+      { id: 4, name: 'まゆたそ' },
+      { id: 5, name: 'あおい' },
     ],
     skipDuplicates: true,
   });
