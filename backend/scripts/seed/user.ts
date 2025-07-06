0e42937dff693171743a6ba34e831337596c1323
kmaruoka <k.m.igosso@gmail.com>
2025-07-06 16:38:49 +0900

    feat: show titles and ability management

--- backend/scripts/seed/user.ts
+++ backend/scripts/seed/user.ts
@@ -3,11 +3,11 @@ import { PrismaClient } from '@prisma/client';
 export async function seedUser(prisma: PrismaClient) {
   await prisma.user.createMany({
     data: [
-      { id: 1, name: 'まるにぃ' },
-      { id: 2, name: 'たかやん' },
-      { id: 3, name: 'ダイナマイト古川' },
-      { id: 4, name: 'まゆたそ' },
-      { id: 5, name: 'あおい' },
+      { id: 1, name: 'まるにぃ', level: 0, exp: 0, ability_points: 0 },
+      { id: 2, name: 'たかやん', level: 0, exp: 0, ability_points: 0 },
+      { id: 3, name: 'ダイナマイト古川', level: 0, exp: 0, ability_points: 0 },
+      { id: 4, name: 'まゆたそ', level: 0, exp: 0, ability_points: 0 },
+      { id: 5, name: 'あおい', level: 0, exp: 0, ability_points: 0 },
     ],
     skipDuplicates: true,
   });
