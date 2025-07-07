10f122fcd51ad8ec5347eb5c09f6397e6d06d889
kmaruoka <k.m.igosso@gmail.com>
2025-07-08 02:12:39 +0900

    feat(frontend): add barrier selection

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import './index.css';
 import './App.css';
 import App from './App.tsx';
 import { SkinProvider } from './skins/SkinContext';
+import { BarrierProvider } from './barriers/BarrierContext';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <SkinProvider>
-        <App />
+        <BarrierProvider>
+          <App />
+        </BarrierProvider>
       </SkinProvider>
     </QueryClientProvider>
   </StrictMode>,
