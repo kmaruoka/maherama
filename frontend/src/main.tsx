87d78dd4c4b624a62eb94e5fc4842e13ec2193ae
kmaruoka <k.m.igosso@gmail.com>
2025-07-26 11:56:12 +0900

    fix: styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import './App.css';
 import App from './App.tsx';
 import { SkinProvider } from './skins/SkinContext';
 import { BarrierProvider } from './barriers/BarrierContext';
